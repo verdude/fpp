65252309f67dd5bdf9f1eea70a5002e7c074fd98
Santiago Verdu <erra@qmake.org>
2025-08-27 15:33:25 -0600

    simpler

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const Args = @import("args.zig");
 
 const Interface = struct {
     writer: *std.Io.Writer,
@@ -11,7 +10,7 @@ const Interface = struct {
 
     pub fn greet(i: *Interface) !void {
         try i.vtable.hello(i);
-        _ = try i.writer.write("\nInterface Method Write\n");
+        _ = try i.writer.write("Interface Method Write\n");
     }
 
     pub fn flush(i: *Interface) !void {
@@ -20,21 +19,19 @@ const Interface = struct {
 };
 
 const Impl = struct {
+    slice: []const u8 = "burger",
     interface: Interface,
-    number2: u16 = 0,
-    poopoo: ?[]const u8 = null,
 
     fn hello(i: *Interface) !void {
         const self: *Impl = @alignCast(@fieldParentPtr("interface", i));
-        try i.writer.print("{s}", .{self.poopoo orelse "placeholder"});
-        _ = try i.writer.write(" - Oh I love agriculture. ");
+        _ = try i.writer.write("Oh I love agriculture.\n");
         try i.writer.print(
-            "\n{*}\n{*}\n{*}\n{*}\n",
+            "{*}\n{*}\n{*}\n{*}\n",
             .{
+                i,
                 self,
-                &self.poopoo,
-                &self.number2,
                 &self.interface,
+                &self.slice,
             },
         );
     }
@@ -56,16 +53,12 @@ const Impl = struct {
 };
 
 pub fn main() !void {
-    var args = Args.init();
-    try args.parse();
-    const dir = std.fs.cwd();
-    var writer: std.fs.File.Writer = undefined;
-    if (args.safe) {
-        const file = try dir.createFile("output.txt", .{});
-        var buf: [1024]u8 = undefined;
-        writer = file.writer(&buf);
-    }
+    const file = try std.fs.cwd().createFile("output.txt", .{});
+
+    var buf: [1024]u8 = undefined;
+    var writer = file.writer(&buf);
     var impl = Impl.init(&writer.interface);
-    try impl.interface.greet();
+
+    try (impl.interface).greet();
     try impl.flush();
 }
